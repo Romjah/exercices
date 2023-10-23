7fbb38937baed2ac882e1fd3e8ecfd83e1e9195c
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2023-10-23 14:55:49 +0200

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,14 @@
-import React, { useState } from 'react';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import MenuHamburger from '../components/MenuHamburger'; // 1. Importez le composant MenuHamburger
 
 const Home = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  const exerciseRoutes = [
-    'todolist',
-    'compteur',
-    'profil',
-    'navigation',
-    'liste_de_produit',
-    'recherche_de_films',
-    'pagination',
-    'filter',
-  ];
-
   return (
     <div className="App-header">
-      {/* Icône hamburger */}
-      <div style={{ position: 'absolute', top: '20px', left: drawerOpen ? '250px' : '20px' }}>
-        <IconButton onClick={toggleDrawer} edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-      </div>
+      <MenuHamburger />  {/* 2. Utilisez le composant MenuHamburger ici */}
 
       {/* Titre et sous-titre */}
       <div className="home-title">Bienvenue sur Mon Site</div>
       <div className="home-subtitle">Découvrez nos incroyables fonctionnalités!</div>
-
-      {/* Drawer pour la navigation */}
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
-        <List>
-          {exerciseRoutes.map((exercise, index) => (
-            <ListItem key={index} component={Link} to={`/${exercise}`}>
-              <ListItemText primary={`Exercice ${index + 1}`} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
     </div>
   );
 };
